0389f7e466255973fba3ea59cebbb3297d407c18
Yariv Katz <yariv@yariv-HP-Pavilion-dv6500-Notebook-PC.(none)>
2013-06-12 17:01:24 +0300

    install prerequisites file

---
+++ install-prerequisites.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#this script will install applications needed for our apps in ubuntu
+main{}{
+	#install git
+	sudo apt-get install git
+
+	#install prerequisites for aptana
+	sudo apt-get install openjdk-7-jdk libjpeg62 libwebkitgtk-1.0-0 git-core
+
+	#install virtualenv
+	sudo apt-get install python-virtualenv
+
+	#install heroku toolbelt
+	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+	
+}
+
+main $*
